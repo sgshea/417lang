ac317079fbf53adb61867fb029684aa27dfdb839
sgshea <sgshea@ncsu.edu>
2024-09-10 11:02:42 -0400

    fix: No error on empty input

--- src/reader.rs
+++ src/reader.rs
@@ -8,7 +8,14 @@ use crate::interpreter::InterpError;
 
 pub fn read_stdin() -> Result<Value, InterpError> {
     match serde_json::from_reader(io::stdin()) {
-        Err(_) => Err(InterpError::ParseError("Invalid JSON AST".to_string())),
+        Err(e) => {
+            // We don't want to error on empty input
+            if e.is_eof() {
+                Ok(Value::String("".to_string()))
+            } else {
+                Err(InterpError::ParseError(e.to_string()))
+            }
+        }
         Ok(ok) => Ok(ok),
     }
 }
